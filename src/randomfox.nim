b3b6a7f440579dfa023424cba0c9cba8476f704e
in my mind... <94906343+l0v3m0n3y@users.noreply.github.com>
2025-11-13 15:29:20 +0300

    Create randomfox.nim

---
+++ src/randomfox.nim
@@ -0,0 +1,19 @@
+import asyncdispatch, httpclient, json, strutils, uri
+
+const api = "https://randomfox.ca"
+var headers = newHttpHeaders({
+    "Connection": "keep-alive",
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36",
+    "Host": "randomfox.ca",
+    "Accept": "application/json"
+})
+
+proc random_fox*(): Future[JsonNode] {.async.} =
+  let client = newAsyncHttpClient()
+  client.headers = headers
+  try:
+    let response = await client.get(api & "/floof/")
+    let body = await response.body
+    result = parseJson(body)
+  finally:
+    client.close()
